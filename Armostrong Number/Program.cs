0e2e5845dd3a0f0de5a1203f81f1d74c2194b18a
user <user@DESKTOP-VLDFR9D>
2023-02-03 19:56:20 +0530

    Add project files.

---
+++ Armostrong Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Armostrong_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!! Enter the number to check");
+            string n = Console.ReadLine();
+            int num = Int32.Parse(n);
+            while(num%10 == 0) { 
+
+            }
+        }
+    }
+}
